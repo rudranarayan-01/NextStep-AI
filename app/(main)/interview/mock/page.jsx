75e08b47b3eba0bf70770021179361d3eb4985c0
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-06-03 11:51:17 +0530

    cover letter

---
+++ app/(main)/interview/mock/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MockPage = () => {
+  return (
+    <div>MockPage</div>
+  )
+}
+
+export default MockPage
---
+++ app/(main)/interview/page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MockPage from './mock/page'
+
+const InterviewPage = () => {
+  return <MockPage/>
+}
+
+export default InterviewPage
