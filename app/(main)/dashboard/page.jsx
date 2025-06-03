f22eb3617201f0d4cfd2e70391a7b86a452ad788
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-06-03 19:44:24 +0530

    onboarding

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { getIndustryInsights } from "@/actions/dashboard";
+import DashboardView from "./_component/dashboard-view";
+import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
 
-const DashboardPage = () => {
+export default async function DashboardPage() {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  // If not onboarded, redirect to onboarding page
+  // Skip this check if already on the onboarding page
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
+
+  const insights = await getIndustryInsights();
+
   return (
-    <div>DashboardPage</div>
-  )
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
+    </div>
+  );
 }
-
-export default DashboardPage
